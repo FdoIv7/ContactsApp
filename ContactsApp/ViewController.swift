356cbee1b6e5dfc39325bce717f0654b3f1f3469
Fer <fernando@ordering.co>
2021-12-08 21:11:59 -0600

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Fernando Pérez Ruiz on 08/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
